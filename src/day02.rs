dcaec4453234459abd3f33001a22bfe6ce81cd31
Dominik Baumann <hey@sobrinth.com>
2025-12-02 08:27:39 +0100

    Day2, part1

---
+++ src/day02.rs
@@ -0,0 +1,30 @@
+pub fn part_1(input: &str) -> i64 {
+    input
+        .split(",")
+        .map(|r| r.split_once("-").unwrap())
+        .map(|(l, r)| (l.parse::<i64>().unwrap(), r.parse::<i64>().unwrap()))
+        .fold(0, |mut acc, (start, end)| {
+            for i in start..=end {
+                if i.to_string().len() % 2 == 0 {
+                    let i_str = i.to_string();
+                    let (l, r) = i_str.split_at(i_str.len() / 2);
+                    if l == r {
+                        acc += i
+                    }
+                }
+            }
+            acc
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let s = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+        assert_eq!(part_1(s), 1227775554);
+    }
+}
