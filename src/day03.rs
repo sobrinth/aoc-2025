2841f5b0f53b1eac040ff7c7810ced6725f20819
Dominik Baumann <hey@sobrinth.com>
2025-12-03 08:07:00 +0100

    Day3, part2
    kinda easy with part 1 done :)

--- src/day03.rs
+++ src/day03.rs
@@ -22,6 +22,29 @@ pub(crate) fn part_1(input: &str) -> u32 {
         .sum()
 }
 
+pub(crate) fn part_2(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect_vec())
+        .map(|l| {
+            l.windows(12)
+                .fold([0; 12].to_vec(), |mut v, n| {
+                    for i in 0..12 {
+                        if n[i] > v[i] {
+                            // only replace [i..] in the 'battery' vector with the new window (remaining) values
+                            v.truncate(i);
+                            v.append(&mut n[i..].to_vec());
+                            break;
+                        }
+                    }
+                    v
+                })
+                .iter()
+                .fold(0u64, |acc, n| acc * 10 + (*n as u64))
+        })
+        .sum::<u64>()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,5 +57,15 @@ mod tests {
 818181911112111";
 
         assert_eq!(part_1(s), 357);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let s = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+
+        assert_eq!(part_2(s), 3121910778619);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ aoc_main::main! {
     year 2025;
     day01 => part_1, part_2;
     day02: generate => part_1, part_2;
-    day03 => part_1;
+    day03 => part_1, part_2;
 }
