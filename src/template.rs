5a2f5082ae8ec38d00a6a10d2b1f3fa0c4622888
Dominik Baumann <hey@sobrinth.com>
2025-12-05 11:55:38 +0100

    add day template

---
+++ src/template.rs
@@ -0,0 +1,25 @@
+pub fn part_1(input: &str) -> i32 {
+    0
+}
+
+pub fn part_2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let s = "input";
+
+        assert_eq!(part_1(s), 0);
+    }
+    #[test]
+    fn test_part_2() {
+        let s = "input";
+
+        assert_eq!(part_2(s), 0);
+    }
+}
