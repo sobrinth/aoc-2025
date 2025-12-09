9ff5d8e62dce3ff04676af2805bb42f956e91bea
Dominik Baumann <hey@sobrinth.com>
2025-12-09 08:25:22 +0100

    day9 "domain language"

--- src/day09.rs
+++ src/day09.rs
@@ -1,37 +1,27 @@
 use itertools::Itertools;
 
+#[derive(Debug, Copy, Clone)]
+struct Tile {
+    x: u64,
+    y: u64,
+}
+
 pub fn part_1(input: &str) -> u64 {
-    input
-        .lines()
-        .filter_map(|l| {
-            l.split_once(",").map(|(x, y)| {
-                let x = x.parse::<u64>().unwrap();
-                let y = y.parse::<u64>().unwrap();
-                (x, y)
-            })
-        })
+    parse_tiles(input)
+        .iter()
         .tuple_combinations()
-        .map(|(a, b)| (a.0.abs_diff(b.0) + 1) * (a.1.abs_diff(b.1) + 1))
+        .map(|(a, b)| (a.x.abs_diff(b.x) + 1) * (a.y.abs_diff(b.y) + 1))
         .max()
         .unwrap()
 }
 
 pub fn part_2(input: &str) -> u64 {
-    let mut tiles = input
-        .lines()
-        .filter_map(|l| {
-            l.split_once(",").map(|(x, y)| {
-                let x = x.parse::<u64>().unwrap();
-                let y = y.parse::<u64>().unwrap();
-                (x, y)
-            })
-        })
-        .collect_vec();
+    let mut tiles = parse_tiles(input);
 
     let mut possible_rectangles = Vec::with_capacity(tiles.len() * tiles.len());
 
     tiles.iter().tuple_combinations().for_each(|(&a, &b)| {
-        possible_rectangles.push((a, b, (a.0.abs_diff(b.0) + 1) * (a.1.abs_diff(b.1) + 1)))
+        possible_rectangles.push((a, b, (a.x.abs_diff(b.x) + 1) * (a.y.abs_diff(b.y) + 1)))
     });
 
     // add the first tile to the end to make it easier to "wrap around"
@@ -40,13 +30,18 @@ pub fn part_2(input: &str) -> u64 {
     possible_rectangles
         .iter()
         .map(|(p1, p2, area)| {
-            let [bbx_min, bbx_max, bby_min, bby_max] = bounding_box(p1, p2);
+            /*
+               Check if the rectangle is fully inside the bounding box of the "floor plan"
+               So: the rectangle is not allowed to intersect with any of the lines of the "floor plan"
+               and needs to be fully contained within the bounding box
+            */
+            let [bx_min, bx_max, by_min, by_max] = bounding_box(p1, p2);
             for w in tiles.windows(2) {
                 let [linex_min, linex_max, liney_min, liney_max] = bounding_box(&w[0], &w[1]);
                 // Handle vertical line intersection
                 if linex_min == linex_max {
-                    if linex_min > bbx_min && linex_max < bbx_max {
-                        if !(liney_max <= bby_min || liney_min >= bby_max) {
+                    if linex_min > bx_min && linex_max < bx_max {
+                        if !(liney_max <= by_min || liney_min >= by_max) {
                             return None;
                         }
                     }
@@ -54,8 +49,8 @@ pub fn part_2(input: &str) -> u64 {
 
                 // Handle horizontal line intersection
                 if liney_min == liney_max {
-                    if liney_min > bby_min && liney_max < bby_max {
-                        if !(linex_max <= bbx_min || linex_min >= bbx_max) {
+                    if liney_min > by_min && liney_max < by_max {
+                        if !(linex_max <= bx_min || linex_min >= bx_max) {
                             return None;
                         }
                     }
@@ -68,12 +63,25 @@ pub fn part_2(input: &str) -> u64 {
         .unwrap()
 }
 
-fn bounding_box(p1: &(u64, u64), p2: &(u64, u64)) -> [u64; 4] {
+fn parse_tiles(input: &str) -> Vec<Tile> {
+    input
+        .lines()
+        .filter_map(|l| {
+            l.split_once(",").map(|(x, y)| {
+                let x = x.parse::<u64>().unwrap();
+                let y = y.parse::<u64>().unwrap();
+                Tile { x, y }
+            })
+        })
+        .collect()
+}
+
+fn bounding_box(p1: &Tile, p2: &Tile) -> [u64; 4] {
     [
-        p1.0.min(p2.0),
-        p1.0.max(p2.0),
-        p1.1.min(p2.1),
-        p1.1.max(p2.1),
+        p1.x.min(p2.x),
+        p1.x.max(p2.x),
+        p1.y.min(p2.y),
+        p1.y.max(p2.y),
     ]
 }
 
